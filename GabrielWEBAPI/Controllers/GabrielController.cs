de393aaf9cfc83e79956d4c09840dd8db0474387
Rick Brito <britohl@ceas1.uc.edu>
2026-03-26 13:32:33 -0400

    Second commit - added GabrielController

---
+++ GabrielWEBAPI/Controllers/GabrielController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GabrielWEBAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GabrielController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            var result = new
+            {
+                name = "Gabriel",
+                message = "Hello, this is my WebAPI!"
+            };
+
+            return Ok(result);
+        }
+    }
+}
